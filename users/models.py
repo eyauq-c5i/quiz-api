ea0f8467c4c12d50caf1931ee6cf68bd87d8060e
Jefferson <gentbliss@gmail.com>
2025-12-15 19:48:14 +0000

    Implemented Custom User Model

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('educator', 'Educator'),
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
