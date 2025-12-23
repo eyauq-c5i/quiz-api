468994f68c99fea10fb02f4d215cd11e16d850c7
Jefferson <gentbliss@gmail.com>
2025-12-23 12:58:10 +0000

    Fix Authorization Error

--- quizzes/permissions.py
+++ quizzes/permissions.py
@@ -1,9 +1,12 @@
 from rest_framework.permissions import BasePermission
 
 class IsEducator(BasePermission):
+    """
+    Allows access only to users with role='educator'.
+    """
     def has_permission(self, request, view):
         return (
             request.user and
             request.user.is_authenticated and
-            request.user.groups.filter(name='Educators').exists()
+            request.user.role == 'educator'
         )
